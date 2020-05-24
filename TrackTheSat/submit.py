5db22428561ac2f56fbe789779d23ac4c9d8c107
AdamJayne <adamjayne402@gmail.com>
2020-05-24 04:31:19 -0400

    pushing for swft

--- TrackTheSat/submit.py
+++ TrackTheSat/submit.py
@@ -9,12 +9,46 @@ host = "trackthesat.satellitesabove.me"
 port = 5031
 ticket = b"ticket{sierra76499foxtrot:GPGqaO4Dv2OiJ0iK_8J5FRLjpG-Yk3PPYQHo7NZd3iR9lrkL02eVHfMXnunozCIjdA}"
 
+class MySocket:
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < len(msg):
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
 def netcats(host, port, content):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-    sock.send(content)
-    time.sleep(1)
-    sock.shutdown(socket.SHUT_WR)
+    my_sock = MySocket()
+    my_sock.connect(host, port)
+
+    my_sock.mysend(content)
+    response = my_sock.myreceive()
+    response = response.decode()
+
+    print(response)
 
     response = ''
     while True:
@@ -25,26 +59,14 @@ def netcats(host, port, content):
 
     print(response)
 
-    sock.listen()
     
     # grab values to calculate
     sat, lat, lng, timeer = challenge_parser.parse(response)
     
     response_string = do_calc.solve_things(sat, lat, lng, float(timeer))
 
+    # sock.connect((host, port))
 
-    sock.sendall(response_string.encode("utf-8"))
-
-    sock.shutdown(socket.SHUT_WR)
-    
-    response2 = ''
-    while True:
-        data = sock.recv(1024)
-        if not data:
-            break
-        response2 += data.decode()
-    
-    print(response2)
     print('Closing connection...')
     sock.close()
 
