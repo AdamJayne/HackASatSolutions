1dd5a1c5b6c6feb8cc3fa1cc118fa418eba5a8cf
AdamJayne <adamjayne402@gmail.com>
2020-05-23 23:12:21 -0400

    some changes

--- TrackTheSat/solve.py
+++ TrackTheSat/solve.py
@@ -2,14 +2,24 @@ import datetime
 from skyfield.api import load, EarthSatellite, Topos
 
 """
-Track-a-sat control system
-Latitude: 52.5341
-Longitude: 85.18
-Satellite: PERUSAT 1
-Start time GMT: 1586789933.820023
-720 observations, one every 1 second
-Waiting for your solution followed by a blank line...
+Latitude: 19.8957
+Longitude: 75.3203
+Satellite: MAROC-TUBSAT
+Start time GMT: 1585994946.924365
+"""
 
+"""
+duty cycles between 2457 and 7372, from 0 to 180 degrees
+"""
+
+""" az , elev
+N - 2457, 2457
+E - 
+
+"""
+
+"""
+ds = az * ((7372-2157)/180) + 2457
 """
 
 def build_ts(time_num):
@@ -20,7 +30,7 @@ def build_sat(sat_name):
     return EarthSatellite(tle1, tle2, sat_name)
 
 def build_topos(lat, lon):
-    return Topos(lat, lon)
+    return Topos(latitude_degrees=lat, longitude_degrees=lon)
 
 sat_dict = {}
 
@@ -29,13 +39,13 @@ with open('Data/active.txt','r') as f:
     data = [l.replace('\n', "").rstrip() for l in data]
 
 for index, line in enumerate(data[::3]):
-    sat_dict[line] = data[index],data[index+1]
+    sat_dict[line] = data[index-1],data[index]
 
 # Setup
-lat = 52.5341
-lon = 85.18
-demo_time = 1586789933.820023
-sat_name = "PERUSAT 1"
+lat = 19.8957
+lon = 75.3203
+demo_time = 1585994946.924365
+sat_name = "MAROC-TUBSAT"
 
 # Building sat / topos object
 bluffton = build_topos(lat, lon)
@@ -47,3 +57,8 @@ print(ts)
 
 difference = sat - bluffton
 
+topocentric = difference.at(ts)
+
+alt, az, distance = topocentric.altaz()
+
+print(alt.degrees, az.degrees, distance.m)
